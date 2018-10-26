b8af1db1a720a102266bde8bfb0f5d4ec7d63cb7
5ExAimbot <43265447+5ExAimbot@users.noreply.github.com>
2018-10-26 14:37:26 +0530

    Update Basic_11.c

--- Basic_11.c
+++ Basic_11.c
@@ -1,3 +1,26 @@
 // TASK
 // Write code to perform arithmetic operations using a switch case
 // Code Below
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int a=24,b=96,sum,product,difference,quotient,remainder,choice=1;
+ switch (choice)
+ {case 1:sum=a+b;
+         printf("%d is the sum",sum);
+         break;
+  case 2:if (a<b)
+          difference=a-b;
+         else if (b>a)
+          difference=b-a;
+         printf("%d is the differnce",difference);
+         break;    
+  case 3:quotient=a/b;
+         printf("%d is the quotient",quotient);
+         break; 
+  case 4:remainder=a%b;
+         printf("%d is the remainder",remainder);
+         break;   
+ }
+getch();
+}
