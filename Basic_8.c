089a7202a72ed4f74c77ef25e9a8e8000db5aff4
5ExAimbot <43265447+5ExAimbot@users.noreply.github.com>
2018-10-26 14:05:08 +0530

    Update Basic_8.c

--- Basic_8.c
+++ Basic_8.c
@@ -1,3 +1,13 @@
 // TASK
 // Write code to add two 2D Matrix
 // Code Below
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int A[5][4]={{1,2,3,4,5},{6,7,8,9,10},{2,5,7,4,8},{13,23,54,6,7}},B[5][4]={{2,4,6,8,10},{1,3,5,7,9},{14,35,57,68,79},{2,5,76,89,0}},C[5][4],i,j;
+ for (i=0;i<5;i++)
+ {for (j=0;j<4;j++)
+   C[i][j]=A[i][j]+B[i][j];
+ }
+getch();
+} 
