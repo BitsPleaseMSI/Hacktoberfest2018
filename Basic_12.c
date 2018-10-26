caa27645caebc0db3d8c5e907b4bce1ff8b4c1c5
ankitjee2000 <43265538+ankitjee2000@users.noreply.github.com>
2018-10-26 14:20:41 +0530

    Done

--- Basic_12.c
+++ Basic_12.c
@@ -1,3 +1,12 @@
 // TASK
 // Write code to find cube of a number
 // Code Below
+#include<stdio.h>
+void main()
+{ int n,c;
+  printf("Enter the number : ");
+  scanf("%d",n);
+  
+  c=n*n*n;
+  printf("Cube of the %d is %d ",n,c);
+}
