7b925a883faa76d9293f6dfbb84e34a9c5575445
njain794 <44344547+njain794@users.noreply.github.com>
2018-10-26 21:32:22 +0530

    Update Basic_15.c
    code for addition of two numbers in c language

--- Basic_15.c
+++ Basic_15.c
@@ -2,3 +2,17 @@
 // Write code to find sum of 2 integers
 // Take Eg : sum = 344234345 + 324234234
 // Code Below
+#include<stdio.h>
+#include,conio.h>
+void main()
+{
+  clrscr();
+  int a,b,c=0;
+  printf("enter first no. for addition = ");
+  scanf("%d",&a);
+  printf("\nenter second no. for addition = ");
+  scanf("%d",&b);
+  c=a+b;
+  printf("\naddition of two no.'s = %d",c);
+  getch();
+}  
