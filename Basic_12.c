e7b2cd1cbd54f7e2006df861f8842c1396e0907a
njain794 <44344547+njain794@users.noreply.github.com>
2018-10-26 21:25:26 +0530

    Update Basic_12.c
    code to find cube of a no. in c languge

--- Basic_12.c
+++ Basic_12.c
@@ -1,3 +1,15 @@
 // TASK
 // Write code to find cube of a number
 // Code Below
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   clrscr();
+   int a,b=0,c;
+   printf("enter the no to take out its cube = ");
+   scanf("%d",&a);
+   b=a*a*a;
+   printf("cube of no. is = %d",b);
+  getch();
+}  
