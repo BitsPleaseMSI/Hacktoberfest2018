6cde60935e7ba181b0da820571b02f765d3456bd
ankitjee2000 <43265538+ankitjee2000@users.noreply.github.com>
2018-10-26 14:26:27 +0530

    Done

--- Basic_14.c
+++ Basic_14.c
@@ -1,3 +1,16 @@
 // TASK
 // Write code to find the average of the elements in an array
 // Code Below
+#include<stdio.h>
+
+void main()
+{ int i,sum=0,a[5];
+  
+  for(i=0;i<5;++i)
+  scanf("%d",a[i]);
+ 
+ for(i=0;i<5;++i)
+  sum=sum+a[i];
+ 
+ printf("Average of the numbers : %d",sum/5);
+}
