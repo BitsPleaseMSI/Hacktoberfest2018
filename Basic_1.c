6a2dff9cf3099edfcd5706c7573dd0d63d2c3dae
njain794 <44344547+njain794@users.noreply.github.com>
2018-10-26 21:20:09 +0530

    Update Basic_1.c
    code in c language - whether a no. is even or odd

--- Basic_1.c
+++ Basic_1.c
@@ -1,3 +1,21 @@
 // TASK
 // Write code to find if the number is even or odd
 // Code Below
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  int a,b,c;
+  printf("enter the no. to find out whether it is even or odd");
+  scanf("%d",&a);
+  if(a%2==0)
+  {
+    printf("no. is even");
+  }
+  else
+  {
+    printf("no. is odd"0);
+  }
+  getch();
+}
