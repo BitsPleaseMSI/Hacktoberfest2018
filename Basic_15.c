fe012032c4fcb15261682c527c470766f0ec94d6
ankitjee2000 <43265538+ankitjee2000@users.noreply.github.com>
2018-10-26 14:59:33 +0530

    Done

--- Basic_15.c
+++ Basic_15.c
@@ -2,3 +2,12 @@
 // Write code to find sum of 2 integers
 // Take Eg : sum = 344234345 + 324234234
 // Code Below
+#include<stdio.h>
+
+void main()
+{ int a,b;
+ 
+ scanf("%d%d",&a,&b);
+  printf("Sum of the numbers is %d",a+b);
+ 
+}
