87ba9805c7cd9b66d8c1a29050f798bf706af88a
Raghav Thakker <44492657+raghavthakker@users.noreply.github.com>
2018-10-26 14:40:09 +0530

    to differentiate negative or positive

--- Basic_2.c
+++ Basic_2.c
@@ -1,3 +1,18 @@
 // TASK
 // Write code to find if the number is positive or negative
 // Code Below
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int a;
+printf("enter the number");
+scanf(%d,&a);
+  if(a>0)
+    printf("number is positive");
+  else
+    printf("number is negative");
+  getch();
+}
