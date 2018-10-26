3bf130f04de6e296bdab9a4282f7de23733b7871
Raghav Thakker <44492657+raghavthakker@users.noreply.github.com>
2018-10-26 13:58:02 +0530

    Update Challenge_4.c

--- Challenge_4.c
+++ Challenge_4.c
@@ -4,13 +4,17 @@
 
 #include <stdio.h>
 
-int factorial(int num){
-
+int factorial(int num,int a){
+a = a*num;
+    num = num-1;
+    if(num==1)
+        break;
+    factorial(num,a);
 }
 
 int main()
 {
     int num = 10;
-    printf("The factorial of number is 5 is : %d\n",factorial(5));
+    printf("The factorial of number is 5 is : %d\n",factorial(5,1));
     return 0;
 }
