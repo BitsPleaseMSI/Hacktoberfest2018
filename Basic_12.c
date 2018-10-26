cb7b2fd227cdc27a6d093f1733d6462ec0963254
shubhamarya406 <42767393+shubhamarya406@users.noreply.github.com>
2018-10-26 14:11:50 +0530

    update basic_12

--- Basic_12.c
+++ Basic_12.c
@@ -1,4 +1,13 @@
 // TASK
 // Write code to find cube of a number
 // Code Below
-// Just a test
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+   int n;
+  long c;
+  scanf("%d",&n);
+  c=pow(n,3);
+  printf("%ld",c);
+} 
