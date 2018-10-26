49fc580b6c8d2e4a665cfee9974c37395bf35dc4
5ExAimbot <43265447+5ExAimbot@users.noreply.github.com>
2018-10-26 14:21:46 +0530

    Update Basic_3.c

--- Basic_3.c
+++ Basic_3.c
@@ -1,3 +1,18 @@
 // TASK
 // Write code to find if the number is present in the array or not
 // Code Below
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int A[5]={1,4,6,57,34},i,flag=0,notno=6;
+ for (i=0;i<5;i++)
+ {if (no==A[0])
+  {flag=1;
+   break;
+  }
+ }if (flag==1)
+   printf("no. found");
+  else if (flag==0)
+   printf("No. not found");\
+ getch();
+}
