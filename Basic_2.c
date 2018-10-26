6003d8d73e5a85f6adb3c61c7aa6723c8c4bb8cf
5ExAimbot <43265447+5ExAimbot@users.noreply.github.com>
2018-10-26 14:13:32 +0530

    Update Basic_2.c

--- Basic_2.c
+++ Basic_2.c
@@ -1,3 +1,17 @@
 // TASK
 // Write code to find if the number is positive or negative
 // Code Below
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int a=2,b=-1;
+ if (a>0)
+  printf("%d is positive",a);
+ else if (a<0)
+  printf("%d is negative",a);
+ if (b>0)
+  printf("%d is positive",b);
+ else if (b<0)
+  printf("%d is negative",b);
+ getch();
+}
