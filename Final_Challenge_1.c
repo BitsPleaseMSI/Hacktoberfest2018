17bd31aac3066e2d4ff6c2d8a06e5d62aea4ee9b
sumdud3 <42804684+sumdud3@users.noreply.github.com>
2018-10-28 00:09:43 +0530

    completed
    Ex of bubble sort to rearrange elements in Descending order

--- Final_Challenge_1.c
+++ Final_Challenge_1.c
@@ -1,3 +1,52 @@
 //TASK
 //Write code to demonstrate bubble sort
 
+
+/* 
+
+sort to ascending order using 
+bubble sort
+implemented in C
+ 
+*/
+  
+  void main(){
+   int arr[8]= {4,3,5,6,9,34,6,45};     // random jumbled array
+    int i,len=0;
+    
+    len = sizeof(arr[])/sizeof(arr[0]);
+    bubsort(arr[8],len);
+
+   for (i=0,i < len,i++){                // prints result to console
+       printf("%d ", arr[i]);  
+     }
+    }
+ 
+
+int bubsort(int arr[],int len_arr)
+  {
+    int i,j;
+    for(i=0,i<(len_arr-1),i++){
+      
+           for(j=0,j < n-i-1,j++){
+       
+        if(arr[j] < arr[j+1],j++)        // had to google this part
+         swap(&arr[j],&arr[j+1]);
+        
+                                 }
+        
+                             }
+    
+    
+}
+  
+  void swap(int *x, int *y) 
+{ 
+    int k = *x; 
+    *x = *y; 
+    *y = k; 
+} 
+
+
+
+
