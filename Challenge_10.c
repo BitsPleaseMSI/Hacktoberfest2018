938245e59967ec9a0684fe8db86d7f3f107e9949
sashy1212 <72203204+sashy1212@users.noreply.github.com>
2020-10-01 18:26:07 +0530

    Update Challenge_10.c

--- Challenge_10.c
+++ Challenge_10.c
@@ -2,10 +2,17 @@
 // Bubble Sort Algoritm
 
 #include<stdio.h>
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
 int main(){
 
    int count, temp, i, j, number[30];
-
+   printf("Not
    printf("How many numbers are u going to enter?: ");
    scanf("%d",&count);
 
@@ -15,6 +22,13 @@ int main(){
       scanf("%d",&number[i]);
 
    //CODE
+          
+   for (i = 0; i < count-1; i++)       
+  
+       // Last i elements are already in place    
+       for (j = 0; j < count-i-1; j++)  
+           if (number[j] > numbere[j+1]) 
+              swap(&number[j], &number[j+1]); 
 
    printf("Sorted elements: ");
    for(i=0;i<count;i++)
