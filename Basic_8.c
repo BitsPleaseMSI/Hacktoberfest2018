2e56ece57df9766cf440e2adb42f2a31179ea835
Shubham Arya <42767393+shubhamarya406@users.noreply.github.com>
2020-10-02 11:38:36 +0530

    Update Basic_8.c

--- Basic_8.c
+++ Basic_8.c
@@ -1,3 +1,16 @@
 // TASK
 // Write code to add two 2D Matrix
 // Code Below
+void add_mat(int ar[][20],int r1,int c1,int arr[][20],int r2,int c2)
+{
+  if(r1==r2 && c1==c2)
+    {
+      for(int i=0;i<r1;i++)
+      {
+        for(int j=0;j<c1;j++)
+        {
+          cout<<ar[i][j]+arr[i][j];
+        }
+      }
+  }
+}
