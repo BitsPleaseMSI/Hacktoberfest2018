cfaf63a820789642feee149d6402bf7446b476f9
veenaarya603 <72251622+veenaarya603@users.noreply.github.com>
2020-10-02 14:51:46 +0530

    Update Basic_7.c

--- Basic_7.c
+++ Basic_7.c
@@ -2,17 +2,15 @@
 // Write code to reverse a string.
 // Code Below
 import java.util.*;
-
 public class str_rev
 {
   public static void main(String[] args)
   { 
-   String s="Shubham";
+   String s;
+   Scanner sc=new Scanner(System.in);
+   s.sc.nextLine();
    char[] ch=s.toCharArray();
    for(int i=ch.length-1;i>=0;i--)
-   {
     System.out.print(ch[i]);
-   }
   }
 }
-   
