c048aea12d3685292b179096573fb0ddc74fc4fb
shubhamarya605 <56179179+shubhamarya605@users.noreply.github.com>
2020-10-02 12:13:16 +0530

    Update Basic_7.c

--- Basic_7.c
+++ Basic_7.c
@@ -1,3 +1,18 @@
 // TASK
 // Write code to reverse a string.
 // Code Below
+import java.util.*;
+
+public class str_rev
+{
+  public static void main(String[] args)
+  { 
+   String s="Shubham";
+   char[] ch=s.toCharArray();
+   for(int i=ch.length-1;i>=0;i--)
+   {
+    System.out.print(ch[i]);
+   }
+  }
+}
+   
