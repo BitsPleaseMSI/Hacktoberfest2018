6e9e8ae44429c58f3436ab965868695708fb87ab
shubhamarya605 <56179179+shubhamarya605@users.noreply.github.com>
2020-10-02 12:22:04 +0530

    Update Basic_4.c

--- Basic_4.c
+++ Basic_4.c
@@ -1,3 +1,20 @@
 // TASK
 // Write code to perform selection sort
 // Code Below
+
+//This is the function for selection sort
+
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min; 
+    for (i = 0; i < n-1; i++) 
+    {
+        min = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min = j; 
+        arr[min]=arr[min]+arr[i];
+        arr[i]=arr[min]-arr[i];
+        arr[min]=arr[min]-rr[i];
+    } 
+} 
