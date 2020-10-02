e22904b023ac489d4e5cc3e25b7d295dd4d1e1fe
shubhamarya605 <56179179+shubhamarya605@users.noreply.github.com>
2020-10-02 12:02:02 +0530

    Update Basic_6.c

--- Basic_6.c
+++ Basic_6.c
@@ -1,3 +1,14 @@
 // TASK
 // Write code to show a 2D matrix
 // Code Below
+//this is a c++ function to show 2D matrix
+void showMat(int ar[][20],int r,int c)
+{
+  for(int i=0;i<r;i++)
+  {
+    for(int j=0;j<c;j++)
+    {
+      cout<<ar[i][j];
+    }
+  }
+}
