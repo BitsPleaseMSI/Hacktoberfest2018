e5fe6c530ebc40db88b8c1972fbc64f9ad139a90
rakesh745 <72252548+rakesh745@users.noreply.github.com>
2020-10-02 15:12:14 +0530

    Update Basic_10.c

--- Basic_10.c
+++ Basic_10.c
@@ -1,6 +1,7 @@
 // TASK
 // Write code to find transpose of a 2D matrix
 // Code Below
+#include<stdio.h>
 void  transpose(int ar[5][],int r,int c)
 {int i,j;
   for(i=0;i<c;i++)
@@ -11,4 +12,17 @@ void  transpose(int ar[5][],int r,int c)
      }
     printf("\n");
   }
+}
+int main(){
+  int ar[20][20],r,c;
+  scanf("%d %d",&r,&c);
+  for(i=0;i<r;i++)
+  {
+     for(j=0;j<c;j++)
+     { 
+       scanf("%d",ar[i][j]);
+     }
+  }
+  transpose(ar,r,c);
+  return 0;
 }
