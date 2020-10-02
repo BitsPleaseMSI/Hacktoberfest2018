24ff0ff8a9a11c6d3d64b8f2f52f0b890ffa2514
shubhamarya605 <56179179+shubhamarya605@users.noreply.github.com>
2020-10-02 12:24:59 +0530

    Fixed Basic_1.C

--- Basic_1.c
+++ Basic_1.c
@@ -2,14 +2,13 @@
 // Write code to find if the number is even or odd
 // Code Below
 #include<stdio.h>
-void main()
+int main()
 {
    int n;
  scanf("%d",&n);
-  if(n%2==0){
+  if(n%2==0)
     printf("Even");
-  }
-  else{
+  else
     printf("Odd");
-  }
+  return 0; 
 }
